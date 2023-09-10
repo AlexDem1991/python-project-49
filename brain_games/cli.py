d317e9735e5cba87432be8598eb6d10f9ec6bd29
vboxuser <vboxuser@demenin.aleksandr.myguest.virtualbox.org>
2023-09-10 21:04:31 +0700

    add flake8 and make clear code

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,11 +1,14 @@
 import prompt
 
+
 def welcome_user():
-	name = prompt.string("May i have your name? ")
-	return f"Hello, {name}"
+    name = prompt.string("May i have your name? ")
+    return f"Hello, {name}"
+
 
 def main():
-	welcome_user()
+    welcome_user()
 
-if __name__=="__main__":
-	main()
+
+if __name__ == "__main__":
+    main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,15 @@
 from brain_games import cli
 
+
 def greet():
+    """greeting user"""
     print("Welcome to the Brain Games!")
+
 
 def main():
     greet()
     print(cli.welcome_user())
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
