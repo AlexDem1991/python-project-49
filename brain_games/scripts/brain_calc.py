c3fa337bb13df4dbbeea3aa3bfc22d33dda1d54f
vboxuser <vboxuser@demenin.aleksandr.myguest.virtualbox.org>
2023-09-13 21:32:17 +0700

    new game with module brai_games

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,35 @@
+from brain_even import greeting, ending_game
+import random
+import prompt
+
+
+def game():
+    print("What is the result of the expression?")
+    for i in range(3):
+        symbol = random.choice(['*','-','+'])
+        num_1 = str(random.randint(1, 100))
+        num_2 = str(random.randint(1, 100))
+        print(f"Question: {num_1} {symbol} {num_2}")
+        result_str = num_1+symbol+num_2
+        result_int = eval(result_str)
+        answer_user = input("Your answer: ")
+        try:
+            if result_int == int(answer_user):
+                print("Correct!")
+                continue
+            else:
+                return False, result_int, answer_user
+        except ValueError:
+            return False, result_int, answer_user
+    else:
+        return True, result_int, answer_user
+
+
+def main():
+    name = greeting()
+    answer, result_int, answer_user = game()
+    ending_game(name, answer, result_int, answer_user)
+
+
+if __name__ == "__main__":
+    main()
