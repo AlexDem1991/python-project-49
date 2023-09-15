bd41d4c5b02824200d9bd486c439fc4d37fa1550
Aleksandr Demenin <demenin.aleksandr@yandex.ru>
2023-09-15 12:49:30 +0700

    add skcript, make video , biuld program

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/AlexDem1991/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/AlexDem1991/python-project-49/actions)
 https://asciinema.org/a/uHis5JDgSGugLINgnsDMWM4Tm
 https://asciinema.org/a/vdh9kEStbB8d3WRnrAVBeX6Z3
+https://asciinema.org/a/ctrRYNRmgwkukWc8trQS6vOhi
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -17,17 +17,22 @@ def devided(num):
     return result
 
 
+def sorted(num1, num2):
+    if max(num1, num2) % min(num1, num2) == 0:
+        game_answer = min(num1, num2)
+    else:
+        first = devided(num1)
+        second = devided(num2)
+        game_answer = max(list(set(first) & set(second)))
+    return game_answer
+
+
 def game():
     print("Find the greatest common divisir of given numbers.")
     for i in range(3):
-        first_num = random.randint(1,100)
+        first_num = random.randint(1, 100)
         second_num = random.randint(1, 100)
-        if max(first_num, second_num) % min(first_num,second_num) == 0:
-            game_answer = min(first_num,second_num)
-        else:
-            first = devided(first_num)
-            second = devided(second_num)
-            game_answer = max(list(set(first) & set(second)))
+        game_answer = sorted(first_num, second_num)
         print(f"Question: {first_num} {second_num}")
         user_answer = input("Your answer: ")
         try:
@@ -40,7 +45,8 @@ def game():
             return False, game_answer, user_answer
     else:
         return True, game_answer, user_answer
-    
+
+
 def main():
     name = greeting()
     answer, game_answer, user_answer = game()
@@ -49,4 +55,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
