2c4ed4886de14b5e6d8ca643eb60ef1b60d51ca2
Aleksandr Demenin <demenin.aleksandr@yandex.ru>
2023-09-18 17:52:46 +0700

    test

--- games/scripts/brain_games.py
+++ games/scripts/brain_games.py
@@ -1,14 +1,16 @@
 from games import cli
 
 
-#def greet():
-#    """greeting user"""
-   
-
-def main():
+def greet():
+    """greeting user"""
     print("Welcome to the Brain Games!")
-    print(cli.welcome_user())
 
 
-if __name__ == "__main__":
-    main()
+#def main():
+greet()
+cli.welcome_user()
+
+
+#if __name__ == "__main__":
+#    main()
+    
